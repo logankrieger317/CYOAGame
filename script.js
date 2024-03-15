8bfc2657a4315ce4fa4fbac10d678d83b3e7c994
Joyce003 <jpirtle2348@gmail.com>
2024-03-14 20:32:09 -0500

    choice 2

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ alert(enterCabin);
 
 if(enterCabin == true) {
     //We enter the cabin
-    alert("You decide ot enter the cabin and hear a distant eerie sound echoing throughout the house.");
+    alert("You decide to enter the cabin and hear a distant eerie sound echoing throughout the house.");
+    let eerieSoundChoice = prompt("You walk into the cabin to follow the sound and spot a living room where all the furniture is covered up with tarp. You also spot a kitchen and hear what sounds like a cackling old woman. What do you do? Do you pull off the tarp on the furniture, head to the kitchen, or run out of the cabin?");
 }
 else {
     //We don't enter the cabin (enterCabin is false)
-    alert("You don't go into the cabin. Well, goodbye scaredy cat. Game Over.");
+    alert("You don't go into the cabin. Well, you're no fun. Game Over.");
 }
 
